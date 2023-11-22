3651b0c28da431fd77b78af958337f184c0f7a51
miel <m.ferrada09@ufromail.cl>
2023-11-22 14:42:36 -0300

    Prueba

---
+++ app/src/main/java/com/miel/cleanheat/InicioApp.kt
@@ -0,0 +1,11 @@
+package com.miel.cleanheat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InicioApp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_app)
+    }
+}
