cbf7f341be5d499f5b004b80d13a54301af3a7ae
miel <m.ferrada09@ufromail.cl>
2023-11-22 18:25:12 -0300

    Get funcional nuevo (maldigo git)

--- app/src/main/java/com/miel/cleanheat/InicioApp.kt
+++ app/src/main/java/com/miel/cleanheat/InicioApp.kt
@@ -2,20 +2,28 @@ package com.miel.cleanheat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class InicioApp : AppCompatActivity(), Get.OnRequestCompleteListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_app)
+        val estado = findViewById<Button>(R.id.gotoStatus)
         val getAsyncTask = Get(this)
-        getAsyncTask.execute()
+        estado.setOnClickListener {
+            getAsyncTask.execute()
+            val datos = findViewById<TextView>(R.id.data)
+            datos.visibility=View.VISIBLE
+        }
+
     }
 
     override fun onRequestComplete(result: String?) {
         runOnUiThread {
             // Actualizar el TextView con la respuesta del GET
-            val textView = findViewById<TextView>(R.id.textView)
+            val textView = findViewById<TextView>(R.id.data)
             textView.text = result ?: "No se pudo obtener la respuesta"
         }
     }
@@ -23,7 +31,7 @@ class InicioApp : AppCompatActivity(), Get.OnRequestCompleteListener {
     override fun onRequestError(error: String?) {
         runOnUiThread {
             // Manejar el error en el TextView
-            val textView = findViewById<TextView>(R.id.textView)
+            val textView = findViewById<TextView>(R.id.data)
             textView.text = "Error: $error"
         }
     }
